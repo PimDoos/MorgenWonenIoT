ee1fd93044cecf45197ee6d97b8ec4345583c772
Pim <17709721+PimDoos@users.noreply.github.com>
2022-08-09 23:29:48 +0200

    Create const.py

---
+++ onesmart/const.py
@@ -0,0 +1,32 @@
+# Actions
+ACTION_GET = "get"
+ACTION_LIST = "list"
+ACTION_PERFORM = "perform"
+ACTION_SUBSCRIBE = "subscribe"
+
+# Commands
+COMMAND_AUTHENTICATE = "authenticate"
+COMMAND_DEVICE = "device"
+COMMAND_EVENTS = "events"
+COMMAND_METER = "meter"
+COMMAND_PING = "ping"
+COMMAND_PRESET = "preset"
+COMMAND_PRESET_GROUP = "presetgroup"
+COMMAND_ROOM = "room"
+COMMAND_SITE = "site"
+COMMAND_USER = "user"
+
+EVENT_ENERGY_CONSUMPTION = "energy_consumption"
+
+RPC_ACTION = "action"
+RPC_COMMAND = "cmd"
+RPC_DATA = "data"
+RPC_EVENT = "event"
+RPC_RESULT = "result"
+RPC_TRANSACTION = "transaction"
+RPC_VALUES = "values"
+
+# Config
+SOCKET_BUFFER_SIZE = 1024
+MAX_TRANSACTION_ID = 65535
+PING_INTERVAL = 30
